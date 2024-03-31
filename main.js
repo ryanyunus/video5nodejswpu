852ece1ce4aa3f06b239409a4ece44280db76bef
ryanyunus <ryandinus@gmail.com>
2024-03-31 09:31:26 +0700

    video 5 nodejs wpu

---
+++ main.js
@@ -0,0 +1,4 @@
+// console.log("hello world");
+const nama = "Ryan Yunus";
+const cetakNama = (nama) => `Hi, nama saya ${nama}`;
+console.log(cetakNama(nama));
---
+++ satu.js
@@ -0,0 +1,14 @@
+// console.log("hello world");
+// const nama = "Ryan Yunus";
+// const cetakNama = (nama) => `Hi, nama saya ${nama}`;
+// console.log(cetakNama(nama));
+
+// function cetak(nama) {
+//   return "Halo, nama saya ${nama}";
+// }
+// console.log(window.cetak(`ryan`));
+
+function cetak(nama) {
+  return `Halo, nama saya ${nama}`;
+}
+console.log(cetak("ryan"));
